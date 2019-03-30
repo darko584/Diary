0396be2bdc35421e79054aab306349bd7e38acd2
mac <mac@macs-MacBook-Air.local>
2019-03-30 11:24:25 +0545

    SECOND COMMIT

--- Diary.c
+++ Diary.c
@@ -9,6 +9,7 @@ typedef struct Diary{
   // char place[15];
    char note[1];
 }S ;
+S g[10],*gptr;
 
 S Newrecord(S *w);
 void viewrecord();
@@ -18,17 +19,22 @@ void searchrecord();
 void delete();
 void addpassword();
 S func(S *e);
+
 int main()
 {
-  char d[1000];
+  gptr=g;
   S r,*ptr;
-ptr=&r;
-Newrecord(ptr);
-fgets(d,100,stdin);
-
-
+  ptr=&r;
+  switch(getchar())
+  {
+    case '1':
+      Newrecord(ptr);
+    case '2':
+      addrecord();
+  }
 
 }
+
 S Newrecord(S *w)
 {
 printf("Enter the name of the owner\n");
@@ -43,4 +49,28 @@ scanf(" %[^\n]s",w->note);
 //while(scanf(" %[^\n]%*c",w->note)==1){
   //break;
   //}
+}
+
+void addrecord ()
+{
+  int i;
+  FILE *fp;
+  fp=fopen("/Users/mac/documents/Diary_1.txt","w");
+  for(i=0;i<10;i++)
+  {
+    printf("Current Date : mm-dd-yyyy\n");
+    scanf(" %[^\n]s",gptr->date);
+    printf("Current Time : 00:00:00\n");
+    scanf(" %[^\n]s",gptr->time);
+    printf("Note: ");
+    scanf(" %[^\n]s",gptr->note);
+    fprintf(fp,"%s\t%s\t%s\t",gptr->date,gptr->time,gptr->note);
+    gptr++;
+  }
+  fclose(fp);
+}
+
+void editrecord()
+{
+
 }
