4af44fba33dd328d8c627f9ec3560f827b6eef3b
mac <mac@macs-MacBook-Air.local>
2019-03-30 15:40:01 +0545

    Second commit for temp1

--- Diary.c
+++ Diary.c
@@ -14,6 +14,7 @@ int count=0;
 FILE *fp0;
 S g[10],*gptr;
 S r,*ptr;
+S d[10],*dptr;
 
 S Newrecord(S *w);
 void editrecord();
@@ -22,10 +23,11 @@ void searchrecord();
 void delete();
 void addpassword();
 void additionalrecord(FILE *fp1);
+void reader();
 
 void viewrecord()
 {
-  printf("The number of records currently present : %d\n",count);
+  printf("The total number of records currently present : %d\n",count);
 }
 
 void choice()
@@ -34,7 +36,8 @@ void choice()
   1--To add a new record :\t\
   2--To append in the existing record\t\
   3--To exit :\t\
-  4--To view the total no of records : ");
+  4--To view the total no of records :\t\
+  5--To serach records respective to dates : ");
   int d;
   scanf("%d",&d);
   switch(d)
@@ -51,6 +54,9 @@ void choice()
     case 4:
       viewrecord();
       break;
+    case 5:
+      reader();
+      break;
     default:
       printf("Wrong number choice\n");
       break;
@@ -61,6 +67,7 @@ void choice()
 int main()
 {
   gptr=g;
+  dptr=d;
   ptr=&r;
   choice();
 }
@@ -127,5 +134,37 @@ void additionalrecord(FILE *fp1)
     gptr++;
   }
 }
+
+}
+
+void reader()
+{
+  FILE *fp0;
+  int i;
+  fp0=fopen("/Users/mac/documents/Diary_1.txt","r");
+  for(i=0;i<10;i++)
+  {
+  fscanf(fp0,"%s%s%s%s",dptr->name,dptr->date,dptr->time,dptr->note);
+  dptr++;
+  }
+  searchrecord();
+}
+
+void searchrecord()
+{
+  int i;
+  dptr=d;
+  char ch[10];
+  printf("Enter the entry date for the record to be searched : ");
+  scanf(" %[^\n]s",ch);
+    for(i=0;i<10;i++)
+      {
+        if(strcmp(ch,dptr->date)==0)
+        {
+          printf("NAME:%s\tTIME:%s\nNOTE: %s\n ",dptr->name,dptr->time,dptr->note);
+        }
+        dptr++;
+      }
+
 
 }
